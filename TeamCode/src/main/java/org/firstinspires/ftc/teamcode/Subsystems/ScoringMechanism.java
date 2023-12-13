09f359d969dec63fc8929e194db78636b23defbd
KingoftheFlame <matthew.l.mcclure186@gmail.com>
2023-12-13 12:17:00 -0600

    Added Handoff and converted math functions to use Optionals

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ScoringMechanism.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ScoringMechanism.java
@@ -3,49 +3,61 @@ package org.firstinspires.ftc.teamcode.Subsystems;
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
 
-import androidx.annotation.Nullable;
-
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.arcrobotics.ftclib.controller.PIDFController;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.OptionalDouble;
+
 
 public class ScoringMechanism extends SubsystemBase {
     //Outputs
-    private Motor elevatorMotor;
-    private Servo rightServo;
-    private Servo leftServo;
-    private Motor handoffMotor;
+    private final Motor elevatorMotor;
+    private final Servo rightServo;
+    private final Servo leftServo;
+    private final CRServo handoffMotor;
 
     //Inputs
 //    private  armEncoder; //TODO: Find out how to read from external encoders
-    private DigitalChannel magSwitch;
+    private final DigitalChannel magSwitch;
 
 
     //CONSTANTS
-    final double ELEVATOR_MAX_DISTANCE = 850; //In encoder ticks
-    final double ARM_LENGTH = 12.0; //inches
-    final double ELEVATOR_ANGLE = 0.45; //radians
+    private final double ARM_LENGTH = 12.0; //inches TODO
+    private final double ARM_MIN_ANGLE = 0.0; //Radians TODO
+    private final double ARM_MAX_ANGLE = 0.0; //Radians TODO
+    private final double ARM_ANGLE_TO_POSE = 1.0;//TODO
 
+    //Elevator Constants
+    final double ELEVATOR_MAX_DISTANCE = 850; //In encoder ticks //TODO: Make slightly more accurate
+    final double ELEVATOR_ANGLE = 0.45; //radians //TODO
     final double ELEVATOR_DISTANCE_PER_PULSE = 1; //TODO
-    final double STARTING_HEIGHT = 0.0; //inches
+    final double STARTING_HEIGHT = 0.0; //inches // TODO
 
-
+    //Variables
     private double armAngle = 0.0;
-    private PIDFController elevatorController;
-    private Telemetry telemetry;
+    private final PIDFController elevatorController;
+    private double handoffPower = 0.0;
+    private DcMotorSimple.Direction handoffDirection = DcMotorSimple.Direction.REVERSE;
+
+
+    private final Telemetry telemetry;
 
 
     public ScoringMechanism(HardwareMap hardwareMap, Telemetry telemetry){
         elevatorMotor = new Motor(hardwareMap,"Elevator Motor");
         rightServo = hardwareMap.get(Servo.class,"Right Arm Servo");
         leftServo = hardwareMap.get(Servo.class,"Left Arm Servo");
-//        handoffMotor = hardwareMap.get(DcMotor.class,"scoring.motor.handoff");
+        handoffMotor = hardwareMap.get(CRServo.class,"Handoff");
 
 
         magSwitch = hardwareMap.get(DigitalChannel.class,"Mag Switch");
@@ -60,6 +72,7 @@ public class ScoringMechanism extends SubsystemBase {
         elevatorMotor.setRunMode(Motor.RunMode.RawPower);
         elevatorMotor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
         elevatorController = new PIDFController(0.005,0.0,0.00,0.0);
+
 
     }
 
@@ -83,33 +96,47 @@ public class ScoringMechanism extends SubsystemBase {
 
 
         //Arm Movement
-        double position = armAngle / (2 * Math.PI);
+        OptionalDouble position = getServoPoseFromAngle(armAngle);
 
-        leftServo.setPosition(position);
-        rightServo.setPosition(position);
+        if(position.isPresent()) {
+            leftServo.setPosition(position.getAsDouble());
+            rightServo.setPosition(position.getAsDouble());
+        }
 
-
-        telemetry.addLine(String.format("Pos:%d",elevatorMotor.getCurrentPosition()));
-        telemetry.addLine(String.format("Mag: %b", !magSwitch.getState()));
-        telemetry.update();
+        //Handoff
+        handoffMotor.setPower(handoffPower);
+        handoffMotor.setDirection(handoffDirection);
 
     }
 
+    //Setters
     public void setTargetState(double elevatorLength, double armAngle){
         this.armAngle = armAngle;
         elevatorController.setSetPoint(elevatorLength);
     }
-
-
-
-    public void setTargetEndPoint(double distance, double height){
-        double[][] stateOptons =  getStateOptionsFromTargetPoint(distance,height);
+    public void setHandoffState(double power, DcMotorSimple.Direction direction){
+        handoffPower = power;
+        handoffDirection = direction;
     }
+    public boolean setTargetEndPoint(double distance, double height){
+        Optional<double[]> stateOptional =  getStateFromTargetPoint(distance,height);
 
+
+        if(!stateOptional.isPresent()) return false;
+
+        double[] state = stateOptional.get();
+
+        elevatorController.setSetPoint(state[0]);
+        armAngle = state[1];
+
+        return true;
+    }
     public void resetElevatorEncoder(){
         elevatorMotor.resetEncoder();
     }
 
+
+    //Getters
     public double getElevatorLength(){
         return elevatorMotor.getCurrentPosition() * ELEVATOR_DISTANCE_PER_PULSE;
     }
@@ -130,10 +157,19 @@ public class ScoringMechanism extends SubsystemBase {
         return elevatorDistance + armDistance;
     }
 
+    private OptionalDouble getServoPoseFromAngle(double angle){
+        if(angle > ARM_MAX_ANGLE  || angle < ARM_MIN_ANGLE){
+            return OptionalDouble.empty();
+        }
+        return OptionalDouble.of(
+                angle * ARM_ANGLE_TO_POSE
+        );
+    }
 
 
-    @Nullable
-    public double[][] getStateOptionsFromTargetPoint(double distance,double height){
+    //Arm State from target height and distance of end-effector
+
+    private Optional<double[]> getStateFromTargetPoint(double distance, double height){
         /*  Potential Points for Joint from end Point are on:
             - (x-d)^2 + (y - h)^2 = ARM_LENGTH^2 (Circle Formula)
             Potential Points on Elevator:
@@ -149,54 +185,86 @@ public class ScoringMechanism extends SubsystemBase {
          */
 
         double h = height - STARTING_HEIGHT;
-        double d = distance;
 
         final double m = Math.tan(ELEVATOR_ANGLE);
         double a = 1 + Math.pow(m,2);
-        double b = -2 * (d + (m*h));
-        double c = -(Math.pow(ARM_LENGTH,2) - Math.pow(h,2) - Math.pow(d,2));
+        double b = -2 * (distance + (m*h));
+        double c = -(Math.pow(ARM_LENGTH,2) - Math.pow(h,2) - Math.pow(distance,2));
         double[] xCoordinates =  solveQuadratic(a,b,c);
 
-        double[][] possibleStates = {
-                getStateFromJointCordinate(xCoordinates[0],distance),
-                getStateFromJointCordinate(xCoordinates[1],distance)
-        };
 
-        if(possibleStates[0] == null || possibleStates[1] == null) return null;
-        return possibleStates;
+        ArrayList<double[]> possibleStates = new ArrayList<>();
+
+        //Adds
+        getStateFromJointCordinate(xCoordinates[0],distance).ifPresent(possibleStates::add);
+        getStateFromJointCordinate(xCoordinates[1],distance).ifPresent(possibleStates::add);
 
 
+
+        return getBestStatePossibility(possibleStates);
     }
-
-    @Nullable
-    private double[] getStateFromJointCordinate(double x, double targetX){
+    private Optional<double[]> getStateFromJointCordinate(double x, double distance){
         //Get using pythagorean theorem
         double elevatorDistance = Math.sqrt(Math.pow(x,2) + Math.pow(x * Math.tan(ELEVATOR_ANGLE),2));
 
-        // sin(armAngle) = (y - targetY) / ARM_LENGTH
-        double armAngle = Math.acos( (x - targetX) / ARM_LENGTH);
+        // cos(armAngle) = (x - targetX) / ARM_LENGTH
+        double armAngle = Math.acos( (x - distance) / ARM_LENGTH); //TODO: Understand this better
 
-        if(elevatorDistance == Double.NaN || armAngle == Double.NaN) return null;
+        if(Double.isNaN(elevatorDistance) || Double.isNaN(armAngle)) return Optional.empty();
 
-        return new double[] {
+        return Optional.of(new double[] {
                 elevatorDistance,
                 armAngle,
-        };
+        });
     }
 
-    @Nullable
+
     private  double[] solveQuadratic(double a, double b, double c){
         double[] solutions = {0.0,0.0};
         solutions[0] = (-b + Math.sqrt( Math.pow(b,2) - (3*a*c) )) / (2*a);
         solutions[1] = (-b - Math.sqrt( Math.pow(b,2) - (3*a*c) )) / (2*a);
-        if(solutions[0] == Double.NaN || solutions[1] == Double.NaN) return null;
+
         return solutions;
     }
 
+    private Optional<double[]> getBestStatePossibility(ArrayList<double[]> states){
+        ArrayList<double[]> possibleStates = new ArrayList<>();
+
+        for(double[] state : states){
+            //Checks if state is null
+            if(Double.isNaN(state[0]) || Double.isNaN(state[1])) continue;
+
+            //Checks that Elevator State is within min/max distance
+            if( state[0] < 0 || state[0] > ELEVATOR_MAX_DISTANCE) continue;
+
+            if(state[1] < ARM_MIN_ANGLE || state[1] > ARM_MAX_ANGLE) continue;
+
+            //TODO: Guarantee that there is only ever one or less states
+
+            possibleStates.add(state);
+        }
+
+        if(possibleStates.size() == 1) return Optional.of(possibleStates.get(0));
+        else return Optional.empty();
+    }
+
+    /* Elevator->Arm joint position solution
+        If given a target distance and height for the end point
+        - draw a circle of radius = ARM_LENGTH around the end point
+        - draw a straight line at the angle of the elevator
+        - There will only ever be 2 points at max where the line intersects the circle
+        - These intersections are the possible locations of the arm elevator connection
+        - This intersection gives you the length of the elevator and the angle of the arm
 
 
+        The points are the intersections between these two equations
+        - (x-distance)^2 + (y-height)^2 = ARM_LENGTH^2 (note: renamed circle equation)
+        - tan(ELEVATOR_ANGLE)x + ELEVATOR_START_HEIGHT
 
-    //Determining fastest position;
+     */
+
+
+    //Determining fastest position brainstorming;
     /*
     knowns: ARM_SPEED, ELEVATOR_SPEED, height, distance
 
@@ -222,20 +290,6 @@ public class ScoringMechanism extends SubsystemBase {
      */
 
 
-    /*
-        If given a target distance and height for the end point
-        - draw a circle of radius = ARM_LENGTH around the end point
-        - draw a straight line at the angle of the elevator
-        - There will only ever be 2 points at max where the line intersects the circle
-        - These intersections are the possible locations of the arm elevator connection
-        - This intersection gives you the length of the elevator and the angle of the arm
-
-
-        The points are the intersections between these two equations
-        - (x-distance)^2 + (y-height)^2 = ARM_LENGTH^2 (note: renamed circle equation)
-        - tan(ELEVATOR_ANGLE)x + ELEVATOR_START_HEIGHT
-
-     */
 
 
 }
